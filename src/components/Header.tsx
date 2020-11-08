95bc219418e04e252741f4f70f103619ca5cead0
ulydev <ulydev@live.fr>
2020-11-08 20:49:12 +0100

    add Settings screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,13 @@ import { useWeb3React } from "@web3-react/core"
 import { useCapBalance } from "../hooks/useCapBalance"
 
 import logo from "../images/logo.svg"
-import { AiOutlinePlusCircle, AiOutlinePoweroff } from "react-icons/ai"
+import { AiOutlinePlusCircle, AiOutlinePoweroff, AiOutlineSetting } from "react-icons/ai"
 import { getTokenContract, useContract } from "../hooks/useContract"
 import { useStoreActions } from "../state/hooks"
 import { toast } from "react-toastify"
 import { formatAccount } from "../util/formatAccount"
 import { ChainId } from "@uniswap/sdk"
+import { Link, NavLink } from "react-router-dom"
 
 const Account = () => {
     const { account, deactivate, library } = useWeb3React()
@@ -32,7 +33,7 @@ const Account = () => {
     }
 
     return (
-        <div className="flex flex-col items-start lg:items-end space-y-2">
+        <div className="flex flex-col items-end space-y-2">
             <div className="flex flex-row items-center">
                 <div className="flex flex-row space-x-2 items-center">
                     <span className="text-white font-thin">{ formattedAccount }</span>
@@ -40,12 +41,17 @@ const Account = () => {
                 </div>
             </div>
             <div className="flex flex-row items-center space-x-2 pr-1">
-                <span className="opacity-50 ml-2 lg:ml-0 text-sm">{ capBalance?.toSignificant(4) } CAP</span>
+                <span className="opacity-50 text-sm">{ capBalance?.toSignificant(4) } CAP</span>
                 <button
                     onClick={requestFromFaucet}
                     className="opacity-50 hover:opacity-100 transition duration-200 text-lg">
                     <AiOutlinePlusCircle />
                 </button>
+                <NavLink
+                    to="/settings"
+                    className="opacity-50 hover:opacity-100 transition duration-200 text-lg">
+                    <AiOutlineSetting />
+                </NavLink>
                 <button
                     onClick={deactivate}
                     className="opacity-50 hover:opacity-100 transition duration-200 text-lg">
@@ -59,10 +65,10 @@ const Account = () => {
 const Header = () => {
     const { account } = useWeb3React()
     return <>
-        <header className="flex flex-row mx-8 lg:mx-12 py-8 mb-12 text-white justify-between">
+        <header className="flex flex-row mx-8 lg:mx-12 py-8 mb-12 lg:mb-0 text-white justify-between">
             <div className="flex flex-col space-y-1 items-start">
                 <img src={logo} alt="logo" className="w-16" />
-                <h1 className="text-xl font-thin">Governance</h1>
+                <NavLink to="/" className="text-xl font-thin hover:text-green-500 transition duration-200">Governance</NavLink>
                 { process.env.REACT_APP_CHAIN_ID !== ChainId.MAINNET.toString() ? <span className="text-white text-xs p-0 px-2 bg-green-900 mt-6 inline-block opacity-50">{ ChainId[parseInt(process.env.REACT_APP_CHAIN_ID!)] }</span> : null }
             </div>
             <div className="flex flex-row">
---
+++ src/components/Settings.tsx
@@ -0,0 +1,50 @@
+import { CurrencyAmount, JSBI } from "@uniswap/sdk"
+import { parseEther, parseUnits } from "ethers/lib/utils"
+import React, { useEffect, useMemo, useState } from "react"
+import { useContract } from "../hooks/useContract"
+import { useWeb3Result } from "../hooks/useWeb3Result"
+import { useStoreActions } from "../state/hooks"
+import CurrencyInput from "./CurrencyInput"
+
+const Settings = () => {
+
+    const contract = useContract()
+    const stakedCap = useWeb3Result(async ({ account, library }) => (await contract.balanceOf(account)).toString())
+
+    const [amount, setAmount] = useState<string>()
+    useEffect(() => { if (stakedCap) setAmount(CurrencyAmount.ether(stakedCap).toExact()) }, [stakedCap])
+
+    const addPendingTransaction = useStoreActions(actions => actions.addPendingTransaction)
+    const saveStake = async () => {
+        const cAmount = CurrencyAmount.ether(JSBI.BigInt(parseEther(amount!)))
+        const cStakedCap = CurrencyAmount.ether(stakedCap)
+        if (cAmount.greaterThan(cStakedCap)) {
+            addPendingTransaction((await contract.stakeToVote(cAmount.subtract(cStakedCap).raw.toString())).hash)
+        } else {
+            addPendingTransaction((await contract.releaseStaked(cStakedCap.subtract(cAmount).raw.toString())).hash)
+        }
+    }
+
+    return (
+        <div className="flex flex-col text-gray-500 space-y-8">
+            <div className="flex flex-row justify-between items-center">
+                <div className="flex flex-row items-center">
+                    <h1 className="text-white text-2xl">Settings</h1>
+                </div>
+            </div>
+            <div className="flex flex-col relative">
+                <div className="absolute pattern-dots-sm text-green-500 text-opacity-50 left-0 top-0 w-8 h-8 -ml-4 -mt-1" />
+                <label htmlFor="stakedCap" className="mb-2 flex flex-row justify-between">
+                    <span>Staked CAP</span>
+                    <span className="text-gray-800">Default: 10 CAP</span>
+                </label>
+                <CurrencyInput
+                    className="p-2 px-4 text-white bg-green-900 bg-opacity-25 border-green-500 border-b-2"
+                    name="stakedCap" type="number" value={amount || ""} onChange={setAmount} />
+                <input onClick={saveStake} disabled={!stakedCap || !amount} type="button" value="Save Stake" className="mt-4 p-2 px-4 text-white hover:bg-green-500 bg-transparent border-b-2 border-green-500 transition duration-200" />
+            </div>
+        </div>
+    )
+}
+
+export default Settings
