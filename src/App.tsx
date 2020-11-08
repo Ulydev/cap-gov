c29ff8009d8d7d4a347a7295a200ef7538ad4a16
ulydev <ulydev@live.fr>
2020-11-08 20:49:58 +0100

    add Proposal creation screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { useInactiveListener } from './hooks/useInactiveListener';
 import { useWeb3React } from '@web3-react/core';
 import ActivatePrompt from './components/ActivatePrompt';
 import GlobalModal from './components/GlobalModal';
-import { BrowserRouter as Router, Redirect, Route } from "react-router-dom"
+import { BrowserRouter as Router, Redirect, Route, useParams } from "react-router-dom"
 import Header from './components/Header';
 import NetworkPrompt from './components/NetworkPrompt';
 import BlockNumberUpdater from './components/BlockNumberUpdater';
@@ -19,40 +19,44 @@ import classnames from "classnames"
 import { useStoreState } from './state/hooks';
 import NewestProposals from './components/proposals/NewestProposals';
 import OldestProposals from './components/proposals/OldestProposals';
+import NewProposal from './components/proposals/NewProposal';
 import Container from './components/Container';
 import Footer from './components/Footer';
+import Proposal from './components/proposals/Proposal';
+import ProposalRoute from './components/proposals/ProposalRoute';
+import Settings from './components/Settings';
 
 const ActiveApp = () => {
     const pendingTransactions = useStoreState(state => state.pendingTransactions)
     const margin = useMemo(() => Object.keys(pendingTransactions).length > 0, [pendingTransactions])
     return <>
-        <Router>
-            <ToastContainer
-                position="top-center"
-                className={classnames(margin && "mt-12")}
-                style={{ transition: "margin-top 0.5s" }}
-                toastClassName="px-6 py-1 min-h-1 text-xs"
-                transition={Slide}
-                autoClose={10000}
-                hideProgressBar={true}
-                newestOnTop={false}
-                closeButton={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover />
+        <ToastContainer
+            position="top-center"
+            className={classnames(margin && "mt-12")}
+            style={{ transition: "margin-top 0.5s" }}
+            toastClassName="px-6 py-1 min-h-1 text-xs"
+            transition={Slide}
+            autoClose={10000}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeButton={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover />
 
-            <BlockNumberUpdater />
-            <PendingTransactionsUpdater />
+        <BlockNumberUpdater />
+        <PendingTransactionsUpdater />
 
-            <PendingTransactionsCount />
+        <PendingTransactionsCount />
 
-            <Route path="/" exact component={() => <Redirect to="/newest" />} />
-            <Route path="/newest" exact component={NewestProposals} />
-            <Route path="/oldest" exact component={OldestProposals} />
-        </Router>
-        <GlobalModal />
+        <Route path="/" exact component={() => <Redirect to="/newest" />} />
+        <Route path="/newest" exact component={NewestProposals} />
+        <Route path="/oldest" exact component={OldestProposals} />
+        <Route path="/new" exact component={NewProposal} />
+        <Route path="/proposal/:id" exact component={ProposalRoute} />
+        <Route path="/settings" exact component={Settings} />
     </>
 }
 
@@ -62,20 +66,27 @@ function App() {
     const triedEager = useEagerConnect()
     useInactiveListener(!triedEager)
 
+    const correctNetwork = chainId?.toString() === process.env.REACT_APP_CHAIN_ID
+
     return (
         <StoreProvider store={store}>
-            <Header />
-            <Container className="mb-16" style={{ minHeight: "calc(100vh - 22rem)" }}>
-                { active ? (
-                    chainId?.toString() === process.env.REACT_APP_CHAIN_ID ?
-                        <ActiveApp />
-                    :
-                        <NetworkPrompt />
-                ) : (
-                    <ActivatePrompt />
-                ) }
-            </Container>
-            <Footer />
+            <Router>
+                <div className="relative">
+                    <div className="w-full h-full absolute pattern-dots-sm text-green-500 z-0 pointer-events-none" style={{ opacity: 0.13 }} />
+                    <Header />
+                    <Container className="mb-16" style={{ minHeight: "calc(100vh - 22rem)" }}>
+                        { active ? (
+                            correctNetwork ?
+                                <ActiveApp />
+                            :
+                                <NetworkPrompt />
+                        ) : (
+                            <ActivatePrompt />
+                        ) }
+                    </Container>
+                    <Footer />
+                </div>
+            </Router>
         </StoreProvider>
     );
 }
---
+++ src/components/proposals/NewProposal.tsx
@@ -0,0 +1,128 @@
+import React from "react"
+import classnames from "classnames"
+
+import ProposalsHeader from "./ProposalsHeader"
+
+import { useFieldArray, useForm } from "react-hook-form"
+import { FiMinus, FiPlus, FiPlusCircle } from "react-icons/fi"
+import { useWeb3Result } from "../../hooks/useWeb3Result"
+import { useContract } from "../../hooks/useContract"
+import { Proposal } from "../../hooks/useProposal"
+import { useStoreActions } from "../../state/hooks"
+import { toUtf8Bytes } from "ethers/lib/utils"
+
+type ProposalValues = {
+    description: string
+    discoverabilityPeriod: number
+    operations: {
+        address: string
+        value: string
+        signature: string
+        calldata: string
+    }[]
+}
+
+const ProposalForm = () => {
+    const { register, control, handleSubmit, watch, errors } = useForm<ProposalValues>({ defaultValues: { operations: [{}] } })
+    const { fields: operations, append: addOperation, remove: removeOperation } = useFieldArray({ control, name: "operations" })
+
+    const contract = useContract()
+    const maxOperations: number = useWeb3Result(async ({ account, library }) => (await contract.proposalMaxOperations()).toNumber()) || 1
+
+    const addPendingTransaction = useStoreActions(actions => actions.addPendingTransaction)
+    ;(window as any).contract = contract
+    const onSubmit = async (data: ProposalValues) => {
+        addPendingTransaction((await contract.submitProposal(
+            data.discoverabilityPeriod,
+            data.operations.map(o => o.address),
+            data.operations.map(o => o.value),
+            data.operations.map(o => o.signature),
+            data.operations.map(o => toUtf8Bytes(o.calldata)),
+            data.description
+        )).hash)
+    }
+
+    return (
+        <form className="flex flex-col space-y-8" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col relative">
+                <div className="absolute pattern-dots-sm text-green-500 text-opacity-50 left-0 top-0 w-8 h-8 -ml-4 -mt-1" />
+                <label htmlFor="description" className="mb-2">Description</label>
+                <textarea className="p-2 px-4 text-white bg-green-900 bg-opacity-25 border-green-500 border-b-2" style={{ minHeight: "4rem", maxHeight: "12rem" }} name="description" placeholder="Make the CAP logo pink" ref={register({ required: true })} />
+            </div>
+            <div className="flex flex-col relative">
+                <div className="absolute pattern-dots-sm text-green-500 text-opacity-50 left-0 top-0 w-8 h-8 -ml-4 -mt-1" />
+                <label htmlFor="discoverabilityPeriod" className="mb-2 flex flex-row justify-between">
+                    <span>Discoverability period (# blocks)</span>
+                    <span className="text-gray-800">1 hour ~= 272 blocks</span>
+                </label>
+                <input className="p-2 px-4 text-white bg-green-900 bg-opacity-25 border-green-500 border-b-2" name="discoverabilityPeriod" placeholder="100" type="number" ref={register({ required: true })} />
+            </div>
+            <div className="flex flex-col">
+                <div className="mb-2 flex flex-row justify-between relative">
+                    <div className="absolute pattern-dots-sm text-green-500 text-opacity-50 left-0 top-0 w-8 h-8 -ml-4 -mt-1" />
+                    <span>Operations</span>
+                    <div className="flex flex-row items-center">
+                        <span className="text-gray-800">{ operations.length }</span>
+                        <button
+                            type="button"
+                            className={classnames(
+                                "border-b-2 p-2 ml-4",
+                                operations.length === maxOperations ? "text-gray-800 border-gray-800" : "text-green-500 border-green-500 hover:bg-green-500 hover:text-white transition duration-200"
+                            )}
+                            disabled={operations.length === maxOperations}
+                            onClick={() => { if (operations.length < maxOperations) addOperation({}) }}><FiPlus /></button>
+                    </div>
+                </div>
+                <div className="flex flex-col space-y-4 mt-4">
+                    { operations.map((operation, index) => {
+                        return (
+                            <div key={index} className="flex flex-col border-b-2 border-gray-800">
+                                <div className="flex flex-row items-center justify-between mb-2">
+                                    <h1 className="text-gray-800">Operation #{index+1}</h1>
+                                    <button
+                                        type="button"
+                                        className={classnames(
+                                            "border-b-2 p-2 ml-4",
+                                            operations.length === maxOperations ? "text-gray-800 border-gray-800" : "text-red-500 border-red-500 hover:bg-red-500 hover:text-white transition duration-200"
+                                        )}
+                                        onClick={() => removeOperation(index)}><FiMinus /></button>
+                                </div>
+                                <div className="flex flex-col space-y-2 p-4 pr-0">
+                                    <div className="flex flex-row space-x-2 items-center">
+                                        <label className="flex-1" htmlFor={`operations[${index}].address`}>Address</label>
+                                        <input style={{ flex: 3 }} className="p-2 px-4 text-white bg-green-900 bg-opacity-25 border-green-500 border-b-2" name={`operations[${index}].address`} placeholder="0x0000...0000" ref={register({ required: true })} />
+                                    </div>
+                                    <div className="flex flex-row space-x-2 items-center">
+                                        <label className="flex-1" htmlFor={`operations[${index}].value`}>Value (ETH)</label>
+                                        <input style={{ flex: 3 }} className="p-2 px-4 text-white bg-green-900 bg-opacity-25 border-green-500 border-b-2" name={`operations[${index}].value`} placeholder="0.0" ref={register({ required: true })} />
+                                    </div>
+                                    <div className="flex flex-row space-x-2 items-center">
+                                        <label className="flex-1" htmlFor={`operations[${index}].signature`}>Signature</label>
+                                        <input style={{ flex: 3 }} className="p-2 px-4 text-white bg-green-900 bg-opacity-25 border-green-500 border-b-2" name={`operations[${index}].signature`} placeholder="Signature" ref={register({ required: true })} />
+                                    </div>
+                                    <div className="flex flex-row space-x-2 items-center">
+                                        <label className="flex-1" htmlFor={`operations[${index}].calldata`}>Calldata</label>
+                                        <input style={{ flex: 3 }} className="p-2 px-4 text-white bg-green-900 bg-opacity-25 border-green-500 border-b-2" name={`operations[${index}].calldata`} placeholder="Calldata" ref={register({ required: true })} />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }) }
+                </div>
+            </div>
+            <input type="submit" value="Submit proposal" className="mt-4 p-2 px-4 text-white hover:bg-green-500 bg-transparent border-b-2 border-green-500 transition duration-200" />
+        </form>
+    )
+}
+
+const NewProposal = () => {
+
+    return (
+        <div className="flex flex-col text-gray-500 space-y-8">
+            <ProposalsHeader />
+            <ProposalForm />
+        </div>
+    )
+}
+
+export default NewProposal
