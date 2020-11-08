460ed74a848861aec5b7f5d7e5167e6fe3e33e49
ulydev <ulydev@live.fr>
2020-11-08 20:48:59 +0100

    set fixed position to tx count

--- src/components/PendingTransactionsCount.tsx
+++ src/components/PendingTransactionsCount.tsx
@@ -7,7 +7,7 @@ const PendingTransactionsCount = () => {
     const pendingTransactions = useStoreState(state => state.pendingTransactions)
     const count = Object.keys(pendingTransactions).length
     return (count > 0) ? (
-        <div className="absolute top-0 left-0 right-0 mx-auto w-48 mt-4 text-xs bg-green-500 text-white flex flex-row items-center justify-between p-2 shadow-lg">
+        <div className="fixed z-40 top-0 left-0 right-0 mx-auto w-64 mt-4 text-xs bg-green-500 text-white flex flex-row items-center justify-between p-2 shadow-lg">
             <CgSpinner className="animate-spin mr-1" size="1.2rem" />
             <div className="flex flex-1 flex-row justify-center items-center">{ count } pending transaction{ count > 1 ? "s" : null }</div>
         </div>
